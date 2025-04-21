c4a5540baa8db475c0f8f278cf053b065332a0cc
FLIP865 <148561573+FLIP865@users.noreply.github.com>
2025-04-21 10:16:00 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,21 +7,6 @@
 #include <iso646.h>
 #include "snake.h"
 
-int map_width = INITIAL_MAP_WIDTH;
-int map_height = INITIAL_MAP_HEIGHT;
-int obstacles_x[MAX_OBSTACLES];
-int obstacles_y[MAX_OBSTACLES];
-int num_obstacles = 0;
-char map[MAX_MAP_HEIGHT][MAX_MAP_WIDTH + 1];
-char key;
-char last_key = 'd';
-int stars_collected = 0;
-int speed;
-int x = 5, y = 11;
-int ox, oy;
-int star_x = 8, star_y = 3;
-struct snakestail snakestail_old[MAP_SNAKE_LENGTH];
-
 void init_game_state() {
     stars_collected = 0;
     x = 5;
